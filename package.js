3f9fb1b2b66c3df04447c61c002541bf60873be9
involve-it <arutune@gmail.com>
2019-02-27 11:11:50 +0300

    package changes

--- package.js
+++ package.js
@@ -2,7 +2,7 @@ Package.describe({
   name: 'arutune:bz-engine-ui',
   version: '0.1.0',
   // Brief, one-line summary of the package.
-  summary: 'Encapsulates user live presence, proximity, distance calculations, messaging',
+  summary: 'Encapsulates ui',
   // URL to the Git repository containing the source code for this package.
   git: '',
   // By default, Meteor will default to using README.md for documentation.
@@ -14,8 +14,10 @@ Package.onUse((api) => {
 //api.mainModule("imports/bz-users/index.client.js", "client", { lazy: true });
 //api.mainModule("imports/bz-users/index.shared.js", ["client", "server"], { lazy: true });
 //api.mainModule("imports/bz-users/index.shared.js", ["client", "server"], { lazy: true });
-  api.addFiles([
+  api.addAssets([
     'templates.html',
+  ], 'client')
+  api.addFiles([
     'controller.client.js',
   ], 'client');
 });
